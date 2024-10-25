5a47192bf418cdbc4c7f96d68c493b5fee817721
adityanarayanan343 <adityanarayanan343@gmail.com>
2024-10-25 11:04:02 +0530

    Finished Most of the End Points

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,6 +1,8 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
 const YT_REGEX = new RegExp("^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|embed\/|v\/|.+\?v=)?([a-zA-Z0-9_-]{11})(\S+)?$");
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -17,15 +19,26 @@ export async function POST(req:NextRequest){
             })
         }
         const extractedId = data.url.split("?v=")[1];
-        await prismaClient.stream.create({
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+        const thumbnails = res.thumbnail.thumbnails;
+        thumbnails.sort((a:{width:number},b:{width:number})=>a.width<b.width ?-1:1)
+        const stream = await prismaClient.stream.create({
             data:{userId: data.creatorId,
             url: data.url,
             extractedId: extractedId,
-            type: "Youtube"
+            type: "Youtube",
+            title: res.title ?? "Cant Find Video",
+            smallImg: thumbnails.length > 1?thumbnails[thumbnails.length-2].url:thumbnails[thumbnails.length-1].url?? "https://imgs.search.brave.com/Od1mOiOVIdshqrLcUx0S1-BoN-dMcwU-Rs4XLQVk-uM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvZ29qby1zYXRv/cnUtcGljdHVyZXMt/OHN6NjJobHNnMWVw/bmk2bC5qcGc",
+            bigImg: thumbnails[thumbnails.length-1].url??"https://imgs.search.brave.com/Od1mOiOVIdshqrLcUx0S1-BoN-dMcwU-Rs4XLQVk-uM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvZ29qby1zYXRv/cnUtcGljdHVyZXMt/OHN6NjJobHNnMWVw/bmk2bC5qcGc"
             }
+        })
+        return NextResponse.json({
+            message: "Stream created successfully",
+            id: stream.id
         })
     }
     catch(e){
+        console.log(e);
         return NextResponse.json({
             message: "Error while adding a stream"
         },
